3dcf4e360929f0f5c42eccd61b7f4473e7c6b010
Diuxx <nicolasmarmot@gmail.com>
2021-08-21 01:50:15 +0200

    feat/init: create init function

--- examples/index.ts
+++ examples/index.ts
@@ -2,8 +2,11 @@ import { xJson } from "../src/xJson";
 
 
 (async () => {
-    let movies: xJson = new xJson('examples/databases/movies');
-    
+    let movies: xJson = new xJson('examples/databases/test', 'movies');
+    await movies.init();
+
+    console.log(' -- done --');
+    console.log(movies.data[0]);
 
 
 })();
--- src/xJson.ts
+++ src/xJson.ts
@@ -1,5 +1,10 @@
-import { existsSync, mkdirSync } from "fs";
+import { existsSync, mkdirSync, createReadStream, writeFileSync } from "fs";
+import _, { isNumber } from 'lodash';
 import path, { ParsedPath } from "path";
+
+type Object = {
+    [key: string]: any
+};
 
 /**
  * 
@@ -22,9 +27,12 @@ export class xJson<T = unknown> {
         ext: '.json',
         path: '.',
         // json array key name.
-        key: ''
+        key: '',
+        flags: [],
+        primaryKey: 'id'
     };
     private timer: number = 0;
+    private increment: number = 0;
 
     constructor(file: string, dataName?: string) {
         let parsedPath: ParsedPath = path.parse(path.win32.normalize(`${process.cwd()}/${file}`));
@@ -34,12 +42,59 @@ export class xJson<T = unknown> {
         this.config.key = dataName ?? parsedPath.name;
         this.config.file = path.win32.normalize(`${parsedPath.dir}/${parsedPath.name}${this.config.ext}`);
   
-        if(!existsSync(path.dirname(this.config.file))) {
+        if (!existsSync(path.dirname(this.config.file))) {
             try {
                 mkdirSync(path.dirname(this.config.file), { recursive: true });
             } catch(error) {
                 throw new Error(`unable to create ${path.dirname(this.config.file)} directory.`);
             }
         }
+    }
+
+    public async init(data: T[] = [], flags: Flag[] = [Flag.rule_id_auto_increment]): Promise<void> {
+        const obj: any = {};
+        obj[this.config.key] = data;
+        try {
+            if (!existsSync(this.config.file)) {
+                writeFileSync(this.config.file, JSON.stringify(obj, null, 4), { encoding: 'utf-8' });
+            } 
+            // read file content.
+            let fileContent: any = await this.streamFile(this.config.file);
+            if (!(this.config.key in fileContent)) {
+                throw new Error(`the data key is not the right.`);
+            }
+            this.data = fileContent[this.config.key];
+        } catch(err) {
+            console.log(err);
+            throw new Error(`Unable to read or create ${this.config.file}`);
+        }
+
+        this.config.flags = flags;
+        if (flags.includes(Flag.rule_id_auto_increment)) {
+            this.increment = this.calculateCurrentIncrement();
+            if (!isNumber(this.increment)) {
+                throw new Error('increment is not a number.');
+            }
+        }
+    }
+
+    private async streamFile(file: string): Promise<any> {
+        let stream = createReadStream(file);
+        let chunks: Uint8Array[] = [];
+        return new Promise((resolve, reject) => {
+            stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+            stream.on('err', (err) => reject(err));
+            stream.on('end', () => resolve(JSON.parse(Buffer.concat(chunks).toString('utf-8'))));
+        });
+    }
+
+    private calculateCurrentIncrement(): number {
+        if (this.data.length == 0) {
+            return 1;
+        }
+        if (!(this.data[0] as Object).hasOwnProperty(this.config.primaryKey)) {
+            return 1;
+        }
+        return ((_.maxBy(this.data, (value: any) => value[this.config.primaryKey]) as any)[this.config.primaryKey] || 1) as number;
     }
 }
